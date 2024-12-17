4df036e8ef25da75d8a7a18e28ac366ad468ec16
Alan Olimpio <alancleyton.olimpio@gmail.com>
2024-12-17 20:53:26 -0300

    feat: cria projeto base com Next js e Tailwind CSS

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <>
+      <div className="py-4">
+        <Button>Button</Button>
+      </div>
+    </>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,72 @@
+import Link from "next/link";
+import {
+  Sheet,
+  SheetTrigger,
+  SheetContent,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { ModeToggle } from "./theme-toggle";
+import { MenuIcon } from "./menuIcon";
+import { LogoIcon } from "./LogoIcon";
+
+export default function Header() {
+  return (
+    <header className="flex h-16 w-full items-center border-b border-gray-200 dark:border-gray-800">
+      <div className="mx-auto max-w-7xl flex w-full items-center px-4 md:px-6">
+        <Link href="#" className="mr-6 flex items-center" prefetch={false}>
+          <LogoIcon className="h-5" />
+          <span className="sr-only">Acme Inc</span>
+        </Link>
+
+        <div className="ml-auto flex items-center space-x-4">
+          <nav className="hidden lg:flex items-center space-x-6">
+            <Link
+              href="#"
+              className="text-sm font-medium hover:underline underline-offset-4"
+              prefetch={false}
+            >
+              Home
+            </Link>
+            <Link
+              href="#"
+              className="text-sm font-medium hover:underline underline-offset-4"
+              prefetch={false}
+            >
+              About
+            </Link>
+          </nav>
+          <ModeToggle />
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon" className="lg:hidden">
+                <MenuIcon className="h-6 w-6" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left">
+              <SheetTitle>Menu</SheetTitle>
+              <div className="grid gap-6 p-6">
+                <Link
+                  href="#"
+                  className="text-sm font-medium hover:underline underline-offset-4"
+                  prefetch={false}
+                >
+                  Home
+                </Link>
+                <Link
+                  href="#"
+                  className="text-sm font-medium hover:underline underline-offset-4"
+                  prefetch={false}
+                >
+                  About
+                </Link>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </header>
+  );
+}
