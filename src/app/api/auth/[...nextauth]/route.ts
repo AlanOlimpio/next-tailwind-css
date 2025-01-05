8856c2c36f0d06c913bd425398edce25066fa948
Alan Olimpio <alancleyton.olimpio@gmail.com>
2025-01-05 11:13:24 -0300

    feat: cadastro de usuário

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,43 @@
+import { api } from "@/lib/axios";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      async profile(profile: GoogleProfile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          username: profile.given_name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      await api.post("/api/users", {
+        name: user.name,
+        username: user.username,
+        image: user.image,
+        email: user.email,
+      });
+      return true;
+    },
+    async session({ session, user }) {
+      if (session.user) {
+        return {
+          ...session,
+          user,
+        };
+      }
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -1,17 +1,32 @@
 "use client";
 import { signOut, useSession } from "next-auth/react";
 
+import Image from "next/image";
+
 export function Logout() {
   const { data: session } = useSession();
-  if (session) {
-    return (
+
+  if (!session) {
+    return null;
+  }
+  return (
+    <>
+      {" "}
       <button
         className="text-sm font-medium hover:underline underline-offset-4"
         onClick={() => signOut()}
       >
         Sign out
-      </button>
-    );
-  }
-  return null;
+      </button>{" "}
+      {session.user?.image && (
+        <Image
+          src={session.user?.image}
+          width={24}
+          height={24}
+          alt={session.user?.name}
+          className="!rounded-full  object-cover object-center"
+        />
+      )}
+    </>
+  );
 }
